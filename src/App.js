9025033561dc3b240865421f6eceeceaa2cbaf27
carolina-nobre <carolina.nobre.v@gmail.com>
2019-12-09 17:09:31 -0300

    foi feita a navbar

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavBar from './Componentes/NavBar'
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div>
+      <NavBar/>
+    </div>
+  );
+}
+
+export default App;
