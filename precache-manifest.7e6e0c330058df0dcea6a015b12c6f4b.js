df7bda34b8cde6070261ca6e13eb54a3cdf8cc51
carolina-nobre <carolina.nobre.v@gmail.com>
2019-12-12 14:57:18 -0300

    Updates

---
+++ precache-manifest.7e6e0c330058df0dcea6a015b12c6f4b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48c8bab98d0315bd9180cdb228007632",
+    "url": "/veganese/index.html"
+  },
+  {
+    "revision": "56460bf6ffa108794c3e",
+    "url": "/veganese/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "bfeb528d3bfe8b4c3fbc",
+    "url": "/veganese/static/css/main.017e3614.chunk.css"
+  },
+  {
+    "revision": "56460bf6ffa108794c3e",
+    "url": "/veganese/static/js/2.e9f7789d.chunk.js"
+  },
+  {
+    "revision": "4aa0fe1eaee3dcf45338a38641d40f08",
+    "url": "/veganese/static/js/2.e9f7789d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bfeb528d3bfe8b4c3fbc",
+    "url": "/veganese/static/js/main.350270ec.chunk.js"
+  },
+  {
+    "revision": "2feeded76ba98c1ba31c",
+    "url": "/veganese/static/js/runtime-main.53aef5f2.js"
+  },
+  {
+    "revision": "91f5b5fcd72b004ea53d9ffe70f90696",
+    "url": "/veganese/static/media/caroseul4.91f5b5fc.jpg"
+  }
+]);
